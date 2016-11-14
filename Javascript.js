73b6170c41bc0e007d840e56c31509c6cfcc752d
fishingforth <17svo@cr.k12.ia.us>
2016-11-14 11:45:52 -0600

    Base Canvas of the game section.

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,16 @@
 /**
  * Created by 17svo on 10/24/2016.
  */
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
